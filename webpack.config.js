8352aefea0170a627b57cacfecf8bbf073042e47
snegurova <snegurova@gmail.com>
2021-08-13 22:56:12 +0300

    feat: set up react

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader"]
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: [ "file-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+};
