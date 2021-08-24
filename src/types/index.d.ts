0b76962a16cb2aabdc6634f10f462e6897bf1eb3
snegurova <snegurova@gmail.com>
2021-08-24 23:05:04 +0300

    feat: set eslint

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,6 @@
 declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.ico';
+declare module '*.gif';
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const EslintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 3000
+  }
+};
+
+const eslintPlugin = (isDev) => isDev ? [] : [
+  new EslintPlugin({ extensions: ['ts', 'tsx', 'js'] })
+];
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  devtool: develop ? 'inline-source-map' : false,
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'public'),
@@ -25,13 +42,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ "style-loader", "css-loader"]
+        use: [ MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i,
         // use: [ "file-loader"]
         type: 'asset/resource',
       }
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
     ],
   },
   resolve: {
@@ -42,5 +64,10 @@ module.exports = {
       template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    ...eslintPlugin(develop),
   ],
-};
+  ...devServer(develop)
+});
