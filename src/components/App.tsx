a0f7926c90069b89a58dba4ce33f1a9e32275df5
snegurova <snegurova@gmail.com>
2021-08-25 21:55:57 +0300

    feat: set prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
-import img from './blog-article-image.jpeg';
+import React from "react";
+import "./App.css";
+import img from "./blog-article-image.jpeg";
 
 const App: React.FC = () => (
   <div>
     <h1>React Components!</h1>
+
     <img src={img} alt="" />
   </div>
 );
